9cf2ddcfc818ed91ac00b1232989919d8ea83d87
user <zhensongjianaizi@gmail.com>
2020-09-15 15:01:25 +0900

    homeへのリンクを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/' => 'home#index'
+  root to: 'home#index'
+  # get '/' => 'home#index'
+  # get '/'だとHelperのPathが生成されないのでrootで定義する
   get '/about' => 'home#about'
 end
