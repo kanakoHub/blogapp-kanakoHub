333dcb8d8807d885cf19f6e50332c8f0b29947d0
user <zhensongjianaizi@gmail.com>
2020-09-18 13:04:58 +0900

    new画面の作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root to: 'articles#index'
   # get '/' => 'home#index'
   # get '/'だとHelperのPathが生成されないのでrootで定義する
-  resources  :articles, only: [:show]
+  resources  :articles, only: [:show, :new]
 end
