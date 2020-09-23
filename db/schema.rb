da7435b6e68093dccbb72ae2040690ff00206aaf
user <zhensongjianaizi@gmail.com>
2020-09-23 13:14:43 +0900

    annotateを導入

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_15_212737) do
+ActiveRecord::Schema.define(version: 2020_09_16_034902) do
+
+  create_table "articlenews", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
