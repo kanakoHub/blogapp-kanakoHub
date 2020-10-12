21833d88b79d66e846e513dd118f56e00cc52b0e
user <zhensongjianaizi@gmail.com>
2020-10-12 10:35:27 +0900

    フォロー機能

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,7 +1,7 @@
 class AccountsController < ApplicationController
   def show
     @user = User.find(params[:id])
-    if @user = current_user
+    if @user == current_user
       redirect_to profile_path
     end
   end
