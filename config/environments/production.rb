6c3f07659e829fcb8f1d449091daee67222dfd98
user <zhensongjianaizi@gmail.com>
2020-10-17 14:20:38 +0900

    エラーで追加してみる

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'example.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
