9eb9e4e8d9c500f03fffddee5e778f6fc96f22fb
user <zhensongjianaizi@gmail.com>
2020-10-19 16:59:15 +0900

    module

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,7 @@ Rails.application.routes.draw do
   root to: 'articles#index'
   # get '/' => 'home#index'
   # get '/'だとHelperのPathが生成されないのでrootで定義する
-  resource :timeline, only: [:show]
-
+  
   resources :articles
   
   resources :accounts, only: [:show] do
@@ -18,9 +17,12 @@ Rails.application.routes.draw do
     resources :unfollows, only: [:create]
   end
   
-  resource :profile, only: [:show, :edit, :update]
-  resources :favorites, only: [:index]
-  
+  scope module: :apps do
+    resources :favorites, only: [:index]
+    resource :profile, only: [:show, :edit, :update]
+    resource :timeline, only: [:show]
+  end
+
   namespace :api, defaults: {format: :json} do
     scope '/articles/:article_id' do
       resources :comments, only: [:index, :create]
