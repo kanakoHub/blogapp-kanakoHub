716bc9985560c202c48d1ce2862617d70c8d6fed
user <zhensongjianaizi@gmail.com>
2020-10-20 15:09:41 +0900

    article factoryを登録

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -4,23 +4,7 @@ RSpec.describe Article, type: :model do
   let!(:user) { create(:user) }
 
   context 'タイトルと内容が入力されている場合' do
-    let!(:article) do
-      user.articles.build({
-        title: Faker::Lorem.characters(number: 10),
-        content: Faker::Lorem.characters(number: 300)
-      })
-    end
-
-    # before do
-    #   user = User.create!({
-    #     email: 'test@example.com',
-    #     password: 'password'
-    #   })
-    #   @article = user.articles.build({
-    #     title: Faker::Lorem.characters(number: 10),
-    #     content: Faker::Lorem.characters(number: 300)
-    #   })
-    # end
+    let!(:article) { build(:article, user: user) }
 
     it '記事を保存できる' do
       expect(article).to be_valid
@@ -28,13 +12,12 @@ RSpec.describe Article, type: :model do
   end
   
   context 'タイトルの文字が一文字の場合' do
-    let!(:article) do
-      user.articles.create({
-        title: Faker::Lorem.characters(number: 1),
-        content: Faker::Lorem.characters(number: 300)
-        })
+    let!(:article) { build(:article, title: Faker::Lorem.characters(number: 1), user: user) }
+    
+    before do
+      article.save
     end
-      
+
     it '記事を保存できない' do
       expect(article.errors.messages[:title][0]).to eq('は2文字以上で入力してください')
     end
