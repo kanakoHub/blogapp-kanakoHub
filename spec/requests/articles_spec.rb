2d61c7417c9c220e8ba1f11ce2b6e494c4553c06
user <zhensongjianaizi@gmail.com>
2020-10-20 21:25:10 +0900

    ログインしていない場合のテスト

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe "Articles", type: :request do
       before do
         sign_in user
       end
-
+      
       it "記事が保存される" do
         article_params = attributes_for(:article)
         post articles_path({article: article_params})
@@ -24,6 +24,14 @@ RSpec.describe "Articles", type: :request do
         expect(Article.last.title).to eq(article_params[:title])
         expect(Article.last.content.body.to_plain_text).to eq(article_params[:content])
       end  
+    end
+    
+    context 'ログインしていない場合' do
+      it "ログイン画面に遷移する" do
+        article_params = attributes_for(:article)
+        post articles_path({article: article_params})
+        expect(response).to redirect_to(new_user_session_path)
+      end
     end
   end
 end
