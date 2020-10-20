b92b19300296cb478cc76c244ab544619f2c8f1a
user <zhensongjianaizi@gmail.com>
2020-10-20 20:17:12 +0900

    request spec のファイルを作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,4 @@
 class ArticlesController < ApplicationController
-
     before_action :set_article, only: [:show]
     before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
